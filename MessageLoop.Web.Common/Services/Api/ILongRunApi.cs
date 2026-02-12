1291e9330356be81e22a7dda2a8358f30fcdbc39
dudinda <dudin0da@gmail.com>
2026-02-12 16:57:34 +0100

    added IApiResponse to the long run api

--- MessageLoop.Web.Common/Services/Api/ILongRunApi.cs
+++ MessageLoop.Web.Common/Services/Api/ILongRunApi.cs
@@ -7,12 +7,12 @@ namespace MessageLoop.Master.Services.Api
     public interface ILongRunApi<T> where T : LongRunItem, new()
     {
         [Get("api/v{ver}/{tokenId}")]
-        Task<LongRunResult> GetLongRunStatus([AliasAs("tokenId")]Guid id, [AliasAs("ver")]int version = 1);
+        Task<IApiResponse<LongRunResult>> GetLongRunStatus([AliasAs("tokenId")]Guid id, [AliasAs("ver")]int version = 1);
 
         [Get("api/v{ver}")]
-        Task<List<T>> GetAll([AliasAs("ver")] int version = 1);
+        Task<IApiResponse<List<T>>> GetAll([AliasAs("ver")] int version = 1);
 
         [Patch("api/v{ver}/abort/{tokenId}")]
-        Task Abort([AliasAs("tokenId")] Guid id, [AliasAs("ver")] int version = 1);
+        Task<IApiResponse> Abort([AliasAs("tokenId")] Guid id, [AliasAs("ver")] int version = 1);
     }
 }
