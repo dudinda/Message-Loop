37401308e439ec85395e60a3df0cea2859f7e5f1
dudinda <dudin0da@gmail.com>
2026-09-21 11:09:11 +0200

    added a node api

---
+++ MessageLoop.Node/Services/Api/INodeApi.cs
@@ -0,0 +1,15 @@
+﻿using MessageLoop.Common.Models.LongRun;
+
+using Refit;
+
+namespace MessageLoop.Node.Services.Api
+{
+    public interface INodeApi
+    {
+        [Post("api/v{ver}/demo")]
+        Task<IApiResponse<LongRunToken>> StartLongRunOnSelf([AliasAs("ver")] int version = 1);
+
+        [Post("api/v{ver}/demo")]
+        Task<IApiResponse<LongRunToken>> StartLongRunOnNode([AliasAs("ver")] int version = 1);
+    }
+}
