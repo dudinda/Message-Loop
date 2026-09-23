10f86de8ec834bfee7d89049ce4024ccf04e453d
dudinda <dudin0da@gmail.com>
2026-09-23 19:05:42 +0200

    updated summaries of the long run api

--- MessageLoop.Web.Common/Services/Api/ILongRunApi.cs
+++ MessageLoop.Web.Common/Services/Api/ILongRunApi.cs
@@ -8,21 +8,21 @@ namespace MessageLoop.Web.Common.Services.Api
     {
         /// <summary>
         /// Try to get the status of a long run operation by its token ID.
-        /// <para>See <see cref="MessageLoop.Web.Common.Controllers.v1.LongRunController.GetLongRunStatus(Guid)"/></para>
+        /// <para>See <see cref="Controllers.v1.LongRunController.GetLongRunStatus(Guid)"/></para>
         /// </summary>
         [Get("/api/v{ver}/longrun/{tokenId}")]
         Task<IApiResponse<LongRunResult>> GetLongRunStatus([AliasAs("tokenId")]Guid id, [AliasAs("ver")]int version = 1);
 
         /// <summary>
         /// List all long run operations that are currently in progress or have completed.
-        /// <para>See <see cref="MessageLoop.Web.Common.Controllers.v1.LongRunController.GetAll"/></para>
+        /// <para>See <see cref="Controllers.v1.LongRunController.GetAll"/></para>
         /// </summary>
         [Get("/api/v{ver}/longrun")]
         Task<IApiResponse<List<T>>> GetAll([AliasAs("ver")] int version = 1);
 
         /// <summary>
         /// Abort a long run operation by its token ID.
-        /// <para>See <see cref="MessageLoop.Web.Common.Controllers.v1.LongRunController.Abort(Guid)"/></para>
+        /// <para>See <see cref="Controllers.v1.LongRunController.Abort(Guid)"/></para>
         /// </summary>
         [Patch("/api/v{ver}/longrun/abort/{tokenId}")]
         Task<IApiResponse> Abort([AliasAs("tokenId")] Guid id, [AliasAs("ver")] int version = 1);
