6a4f36391b4dc0cff2d962d78a0da36fded71aa4
dudinda <dudin0da@gmail.com>
2026-09-24 12:22:06 +0200

    added version folder to the node controller

--- MessageLoop.Node/Controllers/v1/NodeController.cs
+++ MessageLoop.Node/Controllers/v1/NodeController.cs
@@ -61,8 +61,7 @@ namespace MessageLoop.Node.Controllers.v1
             {
                 var childTokens = await _schedule.RunOnChildNodes();
                 var results = await _schedule.PollChildNodes(childTokens, cncl);
-
-                return $"Operation completed on {host}".BuildDataTree(results);
+                return$"Operation completed on {host}".BuildDataTree(results);
             });
 
             return Ok(token);
