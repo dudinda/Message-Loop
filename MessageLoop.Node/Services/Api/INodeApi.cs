d8ae4e4ae0c61d5f89d0b7a21ec5b9a4c0558eba
dudinda <dudin0da@gmail.com>
2026-09-25 12:36:30 +0200

    added docs to the node api

--- MessageLoop.Node/Services/Api/INodeApi.cs
+++ MessageLoop.Node/Services/Api/INodeApi.cs
@@ -6,9 +6,19 @@ namespace MessageLoop.Node.Services.Api
 {
     public interface INodeApi
     {
+        /// <summary>
+        /// Start a long-running operation on the current node.
+        /// The operation runs a message loop.
+        ///  <para>See <see cref="Controllers.v1.NodeController.RunOnSelf)"/></para>
+        /// </summary>
         [Post("/api/v{ver}/node/onSelf")]
         Task<IApiResponse<LongRunToken>> RunOnSelf([AliasAs("ver")] int version = 1);
 
+        /// <summary>
+        /// Start a long-running operation on the current node and all child nodes.
+        /// Upon completion, the result shows the exact hierarchy of all external background operations.
+        /// <para>See <see cref="Controllers.v1.NodeController.RunOnNode)"/></para>
+        /// </summary>
         [Post("/api/v{ver}/node/onNode")]
         Task<IApiResponse<LongRunToken>> RunOnNode([AliasAs("ver")] int version = 1);
     }
