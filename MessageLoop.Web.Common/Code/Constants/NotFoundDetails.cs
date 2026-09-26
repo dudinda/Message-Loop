0a94ed03c615dc95970d4b0064607f0d76a63adf
dudinda <dudin0da@gmail.com>
2026-09-26 18:02:45 +0200

    added details to the not found results

---
+++ MessageLoop.Web.Common/Code/Constants/NotFoundDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageLoop.Web.Common.Code.Constants
+{
+    internal class NotFoundDetails
+    {
+        public const string TokenNotFound = "Token Id {0} could not be found.";
+        public const string MessageNotFound = "Message {0} could not be found.";
+        public const string MessageLoopsNotFound = "Message loops with the key {0} could not be found.";
+    }
+}
